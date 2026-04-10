df7ab8ec27dca8739a4820f00df45f93942af9f3
Moshe Rubin <mosherubin137@gmail.com>
2026-04-09 21:24:44 -0500

    Fix ESLint rules

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  {
+    files: ["script.js"],
+    ...js.configs.recommended,
+    languageOptions: {
+      ...js.configs.recommended.languageOptions,
+      globals: {
+        document: "readonly",
+      },
+    },
+  },
+];
